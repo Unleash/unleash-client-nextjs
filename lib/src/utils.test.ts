af4d98fcc5194b5468451d5b81936e1e6d9988a1
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-03 12:19:48 +0100

    update environment variables

--- lib/src/utils.test.ts
+++ lib/src/utils.test.ts
@@ -19,7 +19,7 @@ describe("getDefaultClientConfig", () => {
   it("should use BASE_URL", () => {
     vi.stubGlobal("process", {
       env: {
-        UNLEASH_BASE_URL: "http://example.com/api",
+        UNLEASH_SERVER_API_URL: "http://example.com/api",
       },
     });
 
@@ -29,7 +29,40 @@ describe("getDefaultClientConfig", () => {
       clientKey: "default:development.unleash-insecure-frontend-api-token",
     });
   });
+
+  it("should use set appName", () => {
+    vi.stubGlobal("process", {
+      env: {
+        NEXT_PUBLIC_UNLEASH_APP_NAME: "my-app",
+      },
+    });
+
+    expect(getDefaultClientConfig()).toEqual({
+      url: "http://localhost:4242/api/frontend",
+      appName: "my-app",
+      clientKey: "default:development.unleash-insecure-frontend-api-token",
+    });
+
+    vi.stubGlobal("process", {
+      env: {
+        NEXT_PUBLIC_UNLEASH_APP_NAME: "my-app",
+        UNLEASH_APP_NAME: "my-app-override",
+      },
+    });
+
+    expect(getDefaultClientConfig()).toEqual({
+      url: "http://localhost:4242/api/frontend",
+      appName: "my-app-override",
+      clientKey: "default:development.unleash-insecure-frontend-api-token",
+    });
+  });
 });
+
+// UNLEASH_SERVER_API_URL
+// UNLEASH_FRONTEND_API_URL
+// UNLEASH_SERVER_API_TOKEN
+// UNLEASH_FRONTEND_API_TOKEN
+// UNLEASH_APP_NAME
 
 // export const getDefaultClientConfig = {
 //   url: `${
