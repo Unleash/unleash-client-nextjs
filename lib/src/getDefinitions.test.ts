948097867a794ea22670b6b8aa489d24f36c4140
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-03 13:41:44 +0100

    feat: environment variable config with tests
    BREAKING CHANGE: new names of environment variables

---
+++ lib/src/getDefinitions.test.ts
@@ -0,0 +1,118 @@
+import { getDefinitions } from "./getDefinitions";
+
+const mockFetch = vi.fn();
+const mockConsole = {
+  warn: vi.fn(),
+  error: vi.fn(),
+};
+
+describe("getDefinitions", () => {
+  beforeAll(() => {
+    vi.stubGlobal("fetch", mockFetch);
+    vi.stubGlobal("console", mockConsole);
+  });
+  afterAll(() => {
+    vi.unstubAllGlobals();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.unstubAllEnvs();
+  });
+
+  it("should fetch with default config", () => {
+    mockFetch.mockResolvedValue({ json: () => ({ version: 1, features: [] }) });
+
+    expect(getDefinitions()).resolves.toEqual({
+      version: 1,
+      features: [],
+    });
+
+    expect(mockFetch).toHaveBeenCalledWith(
+      "http://localhost:4242/api/client/features",
+      {
+        headers: {
+          Authorization: "default:development.unleash-insecure-api-token",
+          "Content-Type": "application/json",
+          "UNLEASH-APPNAME": "nextjs",
+          "User-Agent": "nextjs",
+        },
+      }
+    );
+  });
+
+  it("should warn about default config", () => {
+    getDefinitions();
+
+    expect(mockConsole.warn).toHaveBeenCalledWith(
+      "Using fallback Unleash API URL (http://localhost:4242/api).",
+      "Provide a URL or set UNLEASH_SERVER_API_URL environment variable."
+    );
+  });
+
+  it("should show an error when using default token", () => {
+    getDefinitions();
+
+    expect(mockConsole.error).toHaveBeenCalledWith(
+      "Using fallback default token. Pass token or set UNLEASH_SERVER_API_TOKEN environment variable."
+    );
+  });
+
+  it("should take configuration form environment variables", () => {
+    const url = "http://example.com/api";
+    const token = "secure-token";
+    const appName = "my-awesome-app";
+    vi.stubEnv("NEXT_PUBLIC_UNLEASH_SERVER_API_URL", url);
+    vi.stubEnv("UNLEASH_SERVER_API_TOKEN", token);
+    vi.stubEnv("UNLEASH_APP_NAME", appName);
+
+    getDefinitions();
+
+    expect(mockFetch).toHaveBeenCalledWith(`${url}/client/features`, {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+        "UNLEASH-APPNAME": appName,
+        "User-Agent": appName,
+      },
+    });
+
+    expect(mockConsole.warn).not.toHaveBeenCalled();
+    expect(mockConsole.error).not.toHaveBeenCalled();
+  });
+
+  it("is using UNLEASH_SERVER_API_URL and will prioritize it over NEXT_PUBLIC_UNLEASH_SERVER_API_URL", () => {
+    vi.stubEnv("NEXT_PUBLIC_UNLEASH_SERVER_API_URL", "http://example.com/api");
+    vi.stubEnv("UNLEASH_SERVER_API_URL", "http://example.org/api");
+
+    getDefinitions();
+
+    expect(mockFetch).toHaveBeenCalledWith(
+      "http://example.org/api/client/features",
+      expect.anything()
+    );
+  });
+  it("should allow for overriding the default config", () => {
+    const url = "http://example.com/api/client/features";
+    const token = "secure-token";
+    const appName = "my-awesome-app";
+
+    getDefinitions({
+      url,
+      appName,
+      token,
+    });
+
+    expect(mockFetch).toHaveBeenCalledWith(url, {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+        "UNLEASH-APPNAME": appName,
+        "User-Agent": appName,
+      },
+    });
+
+    expect(mockConsole.warn).not.toHaveBeenCalled();
+    expect(mockConsole.error).not.toHaveBeenCalled();
+  });
+});
--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -26,18 +26,36 @@ export const safeCompare = function safeCompare(a: string, b: string) {
 export const randomSessionId = () =>
   `${Math.floor(Math.random() * 1_000_000_000)}`;
 
-export const getDefaultClientConfig = () => ({
-  url: `${
-    process.env.UNLEASH_BASE_URL ||
-    process.env.NEXT_PUBLIC_UNLEASH_BASE_URL ||
-    "http://localhost:4242/api"
-  }/frontend`,
-  appName:
-    process.env.UNLEASH_APP_NAME ||
-    process.env.NEXT_PUBLIC_UNLEASH_APP_NAME ||
-    "nextjs",
-  clientKey:
-    process.env.UNLEASH_FRONTEND_API_TOKEN ||
-    process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_TOKEN ||
-    "default:development.unleash-insecure-frontend-api-token",
-});
+const validateEnviromentVariables = () => {
+  if (process.env.NEXT_PUBLIC_UNLEASH_SERVER_API_TOKEN) {
+    console.warn(
+      "You are trying to set `NEXT_PUBLIC_UNLEASH_SERVER_API_TOKEN`. Server keys shouldn't be public. Use frontend keys or skip `NEXT_PUBLIC_ prefix."
+    );
+  }
+};
+
+const getFrontendUrl = () =>
+  process.env.UNLEASH_FRONTEND_API_URL ||
+  process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_URL;
+
+const getServerBaseUrl = () =>
+  process.env.UNLEASH_SERVER_API_URL ||
+  process.env.NEXT_PUBLIC_UNLEASH_SERVER_API_URL;
+
+export const getDefaultClientConfig = () => {
+  validateEnviromentVariables();
+
+  return {
+    url:
+      getFrontendUrl() ||
+      `${getServerBaseUrl() || "http://localhost:4242/api"}/frontend`,
+    appName:
+      process.env.UNLEASH_APP_NAME ||
+      process.env.NEXT_PUBLIC_UNLEASH_APP_NAME ||
+      "nextjs",
+    clientKey:
+      process.env.UNLEASH_FRONTEND_API_TOKEN ||
+      process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_TOKEN ||
+      "default:development.unleash-insecure-frontend-api-token",
+  };
+};
