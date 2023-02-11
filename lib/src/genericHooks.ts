14708f983c1789957be32033bb66acdcb67d9b40
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-11 15:01:22 +0100

    hooks with typescript generics

---
+++ lib/src/genericHooks.ts
@@ -0,0 +1,12 @@
+import { IToggle, type IVariant } from "unleash-proxy-client";
+import {
+  useFlag as useFlagOriginal,
+  useVariant as useVariantOriginal,
+  useFlags as useFlagsOriginal,
+} from "@unleash/proxy-client-react";
+
+export const useFlag = <T extends string>(name: T) => useFlagOriginal(name);
+export const useVariant = <T extends string, V extends Partial<IVariant>>(
+  name: T
+): V => useVariantOriginal(name) as any;
+export const useFlags = <T extends IToggle[]>() => useFlagsOriginal() as T;
