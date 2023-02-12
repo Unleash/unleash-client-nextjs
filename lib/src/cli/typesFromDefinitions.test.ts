5aa9a199442083007cb43599c0e4dc09f9a61306
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-12 23:05:34 +0100

    update formatting of cli output

--- lib/src/cli/typesFromDefinitions.test.ts
+++ lib/src/cli/typesFromDefinitions.test.ts
@@ -4,8 +4,7 @@ describe("typesFromDefinitions", () => {
   it("should generate types from empty definitions", () => {
     expect(typesFromDefinitions({ version: 1, features: [] }))
       .toMatchInlineSnapshot(`
-        "export type FeaturesVariantMap = {
-        };
+        "export type FeaturesVariantMap = {};
         
         export type FeatureName = keyof FeaturesVariantMap;
         
@@ -76,8 +75,10 @@ describe("typesFromDefinitions", () => {
     ).toMatchInlineSnapshot(`
       "export type FeaturesVariantMap = {
         \\"feature1\\": [{ \\"name\\": \\"variant1\\"; \\"enabled\\": true; \\"payload\\": {\\"type\\":\\"string\\",\\"value\\":\\"variant1\\"} }],
-        \\"feature2\\": [{ \\"name\\": \\"variant1\\"; \\"enabled\\": true; \\"payload\\": {\\"type\\":\\"string\\",\\"value\\":\\"variant1\\"} } |
-          { \\"name\\": \\"variant2\\"; \\"enabled\\": true; \\"payload\\": {\\"type\\":\\"string\\",\\"value\\":\\"variant2\\"} }]};
+        \\"feature2\\": [
+          { \\"name\\": \\"variant1\\"; \\"enabled\\": true; \\"payload\\": {\\"type\\":\\"string\\",\\"value\\":\\"variant1\\"} } |
+          { \\"name\\": \\"variant2\\"; \\"enabled\\": true; \\"payload\\": {\\"type\\":\\"string\\",\\"value\\":\\"variant2\\"} }]
+      };
       
       export type FeatureName = keyof FeaturesVariantMap;
       
@@ -114,7 +115,8 @@ describe("typesFromDefinitions", () => {
       } as any)
     ).toMatchInlineSnapshot(`
       "export type FeaturesVariantMap = {
-        \\"feature1\\": [{\\"name\\":\\"disabled\\",\\"enabled\\":false}]};
+        \\"feature1\\": [{ \\"name\\": \\"disabled\\", \\"enabled\\": false }]
+      };
 
       export type FeatureName = keyof FeaturesVariantMap;
       
