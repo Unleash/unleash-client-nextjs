6c74cd890b0139ee996743042949463630dc5f91
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-13 19:59:12 +0100

    test: flagClient

---
+++ lib/src/flagsClient.test.ts
@@ -0,0 +1,66 @@
+import { flagsClient } from "./flagsClient";
+
+describe("flagsClient", () => {
+  it("should return methods", () => {
+    const client = flagsClient();
+    expect(client).toHaveProperty("isEnabled");
+    expect(client).toHaveProperty("getVariant");
+  });
+
+  it("should return correct values for isEnabled", () => {
+    const client = flagsClient([
+      {
+        name: "foo",
+        enabled: true,
+        variant: { name: "disabled", enabled: false },
+        impressionData: false,
+      },
+      {
+        name: "bar",
+        enabled: false,
+        variant: { name: "disabled", enabled: false },
+        impressionData: false,
+      },
+    ]);
+
+    expect(client.isEnabled("foo")).toBe(true);
+    expect(client.isEnabled("bar")).toBe(false);
+  });
+
+  it("should resolve variant", () => {
+    const client = flagsClient([
+      {
+        name: "foo",
+        enabled: true,
+        variant: {
+          name: "A",
+          enabled: true,
+          payload: {
+            type: "string",
+            value: "FOO",
+          },
+        },
+        impressionData: false,
+      },
+      {
+        name: "bar",
+        enabled: true,
+        variant: { name: "disabled", enabled: false },
+        impressionData: false,
+      },
+    ]);
+
+    expect(client.getVariant("foo")).toEqual({
+      name: "A",
+      enabled: true,
+      payload: {
+        type: "string",
+        value: "FOO",
+      },
+    });
+    expect(client.getVariant("bar")).toEqual({
+      name: "disabled",
+      enabled: false,
+    });
+  });
+});
