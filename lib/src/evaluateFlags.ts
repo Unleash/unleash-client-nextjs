645b7e9fcb43b91d48e7949564e731a796296ab0
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-02-22 19:41:19 +0100

    fix: evaluateFlags context

--- lib/src/evaluateFlags.ts
+++ lib/src/evaluateFlags.ts
@@ -8,18 +8,27 @@ import { ToggleEngine } from "./core/engine";
  */
 export const evaluateFlags = (
   definitions: ClientFeaturesResponse,
-  context: Context
+  context: Context = {}
 ) => {
   const engine = new ToggleEngine(definitions);
   const toggles: IToggle[] = [];
+  const defaultContext: Context = {
+    currentTime: new Date(),
+    appName:
+      process.env.UNLEASH_APP_NAME || process.env.NEXT_PUBLIC_UNLEASH_APP_NAME,
+  };
+  const contextWithDefaults = {
+    ...defaultContext,
+    ...context,
+  };
 
   definitions?.features?.forEach((feature) => {
-    const enabled = engine.isEnabled(feature.name, context);
+    const enabled = engine.isEnabled(feature.name, contextWithDefaults);
     if (!enabled) {
       return;
     }
 
-    const variant = engine.getVariant(feature.name, context);
+    const variant = engine.getVariant(feature.name, contextWithDefaults);
     if (variant.payload === undefined) {
       delete variant.payload; // cleanup before serialization
     }
