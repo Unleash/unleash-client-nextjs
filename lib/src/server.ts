b2d4d2956967b64a5c56b07d174f092dec4bd197
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-05-09 00:06:14 +0200

    add package exports for server-only

---
+++ lib/src/server.ts
@@ -0,0 +1,6 @@
+export * from "./getDefinitions";
+export * from "./evaluateFlags";
+export * from "./utils";
+export * from "./flagsClient";
+export * from "./getFrontendFlags";
+export * from "./types";
