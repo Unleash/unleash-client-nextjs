27adf5d6d55d8a15c81f6f778354ad6e3b65d32b
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-05-08 21:14:21 +0200

    unit tests for unleash client error

--- lib/src/getFrontendFlags.test.ts
+++ lib/src/getFrontendFlags.test.ts
@@ -8,9 +8,17 @@ const mockClient = {
 };
 
 describe("getFrontendFlags", () => {
-  vi.mock("unleash-proxy-client", () => ({
-    UnleashClient: vi.fn().mockImplementation(() => mockClient),
-  }));
+  beforeEach(() => {
+    vi.mock("unleash-proxy-client", () => ({
+      UnleashClient: vi.fn().mockImplementation(() => mockClient),
+    }));
+  });
+
+  afterEach(() => {
+    Object.keys(mockClient).forEach((key) => {
+      mockClient[key as keyof typeof mockClient].mockReset();
+    });
+  });
 
   it("should return a list of toggles", async () => {
     mockClient.getAllToggles.mockReturnValue([]);
@@ -20,15 +28,38 @@ describe("getFrontendFlags", () => {
       }
     });
 
-    const result = await getFrontendFlags();
-    expect(result).toEqual({ toggles: [] });
+    await expect(getFrontendFlags()).resolves.toEqual({ toggles: [] });
     expect(mockClient.start).toHaveBeenCalledOnce();
     expect(mockClient.getAllToggles).toHaveBeenCalledOnce();
-    expect(mockClient.on).toHaveBeenCalledOnce();
-    expect(mockClient.on).toHaveBeenLastCalledWith(
-      "ready",
-      expect.any(Function)
-    );
+    expect(mockClient.on).toHaveBeenCalledWith("ready", expect.any(Function));
     expect(mockClient.stop).toHaveBeenCalledOnce();
+  });
+
+  it("should add error handler", async () => {
+    mockClient.getAllToggles.mockReturnValue([]);
+    mockClient.on.mockImplementation((event, callback) => {
+      if (event === "error") {
+        callback(new Error());
+      }
+    });
+
+    await expect(getFrontendFlags()).rejects.toThrow();
+    expect(mockClient.start).toHaveBeenCalledOnce();
+    expect(mockClient.on).toHaveBeenCalledWith("ready", expect.any(Function));
+    expect(mockClient.stop).toHaveBeenCalledOnce();
+  });
+
+  it("should not hang on error", async () => {
+    let errorCallback = vi.fn();
+    mockClient.on.mockImplementation((event, callback) => {
+      if (event === "error") {
+        errorCallback = callback;
+      }
+    });
+    const pendingPromise = getFrontendFlags();
+
+    errorCallback(new Error("error!"));
+
+    await expect(pendingPromise).rejects.toThrow("error!");
   });
 });
