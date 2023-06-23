f3fef22c76229bbc730a43f58ab4d9152ca5756c
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-06-23 15:23:41 +0200

    feat: add missing react sdk hooks

--- lib/src/hooks.ts
+++ lib/src/hooks.ts
@@ -4,6 +4,8 @@ import {
   useVariant as useVariantOriginal,
   useFlags as useFlagsOriginal,
   useFlagsStatus as useFlagsStatusOriginal,
+  useUnleashContext as useUnleashContextOriginal,
+  useUnleashClient as useUnleashClientOriginal,
 } from "@unleash/proxy-client-react";
 
 export const useFlag = <T extends string>(name: T) => useFlagOriginal(name);
@@ -12,3 +14,5 @@ export const useVariant = <T extends string, V extends Partial<IVariant>>(
 ): V => useVariantOriginal(name) as any;
 export const useFlags = <T extends IToggle[]>() => useFlagsOriginal() as T;
 export const useFlagsStatus = useFlagsStatusOriginal;
+export const useUnleashContext = useUnleashContextOriginal;
+export const useUnleashClient = useUnleashClientOriginal;
