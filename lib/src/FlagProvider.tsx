b214c52d3d86587bcd3adf64b40205a7ff7dadb3
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-07-27 18:50:44 +0200

    fix: flag provider children props

--- lib/src/FlagProvider.tsx
+++ lib/src/FlagProvider.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { ComponentProps, FC } from "react";
+import type { ComponentProps, FC, ReactNode } from "react";
 import { FlagProvider as ReactFlagProvider } from "@unleash/proxy-client-react";
 import { getDefaultClientConfig } from "./utils";
 
 type Config = Required<ComponentProps<typeof ReactFlagProvider>>["config"];
 
 type FlagProviderProps = {
+  children: ReactNode;
   config?: Partial<Config>;
 } & Omit<ComponentProps<typeof ReactFlagProvider>, "config">;
 
