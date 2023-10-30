84a0f0c50ba4a91f24c28e9659058ad32077125b
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-10-30 14:23:58 +0100

    fix: eslint parser

--- common/eslint-config-custom/index.js
+++ common/eslint-config-custom/index.js
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
   },
 };
