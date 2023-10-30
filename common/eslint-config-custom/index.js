b1e562b897938b1e098ed57ec6e1e863d9b30f16
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-10-30 14:29:38 +0100

    fix: variant hashing

--- common/eslint-config-custom/index.js
+++ common/eslint-config-custom/index.js
@@ -3,7 +3,5 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
   },
 };
