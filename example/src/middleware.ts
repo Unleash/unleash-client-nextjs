37a75a0e8ae6113596df7ad6bf3c7d0c0aa01536
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-06-03 12:41:06 +1000

    Setup basic middleware example

--- example/src/middleware.ts
+++ example/src/middleware.ts
@@ -4,40 +4,40 @@ import { addBasePath } from "next/dist/client/add-base-path";
 import { flagsClient, evaluateFlags, randomSessionId } from "@unleash/nextjs";
 
 export const config = {
-    runtime: "experimental-edge",
+  runtime: "experimental-edge",
 };
 
 export async function middleware(req: NextRequest) {
-    const sessionId =
-        req.cookies.get(UNLEASH_COOKIE_NAME)?.value || randomSessionId();
-    let res: NextResponse;
-    if (req.nextUrl.pathname.startsWith('/ab')) {
-        const context = { sessionId }; // You can extend context with other server-side properties
+  const sessionId =
+    req.cookies.get(UNLEASH_COOKIE_NAME)?.value || randomSessionId();
+  let res: NextResponse;
+  if (req.nextUrl.pathname.startsWith("/ab")) {
+    const context = { sessionId }; // You can extend context with other server-side properties
 
-        // Grab definitions from an endpoint cached on the edge
-        const protocol = req.url.startsWith("https") ? "https://" : "http://";
-        const host = req.headers.get("host");
-        const endpoint = addBasePath(UNLEASH_API_PROXY_DEFINITIONS);
-        const token = process.env.UNLEASH_RELAY_SECRET || "";
-        const definitionsUrl = `${protocol}${host}${endpoint}?token=${token}`;
-      
-        // Make a request to the edge-cached endpoint
-        const definitions = await fetch(definitionsUrl).then((res) => res.json());
-      
-        // Evaluate based on provided context
-        const evaluated = await evaluateFlags(definitions, context);
-        const variant = flagsClient(evaluated.toggles).getVariant("nextjs-example")
-          ?.payload?.value;
-      
-        const newUrl = req.nextUrl.clone();
-        // Redirect to variant
-        newUrl.pathname = `/ab/${variant === "a" ? "a" : "b"}`;
-        res = NextResponse.rewrite(newUrl);
-    }
-    else {
-        res = NextResponse.next();
-    }
-    res.cookies.set(UNLEASH_COOKIE_NAME, sessionId);
-    return res; 
+    // Grab definitions from an endpoint cached on the edge
+    const protocol = req.url.startsWith("https") ? "https://" : "http://";
+    const host = req.headers.get("host");
+    const endpoint = addBasePath(UNLEASH_API_PROXY_DEFINITIONS);
+    const token = process.env.UNLEASH_RELAY_SECRET || "";
+    const definitionsUrl = `${protocol}${host}${endpoint}?token=${token}`;
+
+    // Make a request to the edge-cached endpoint
+    const definitions = await fetch(definitionsUrl).then((res) => res.json());
+
+    // Evaluate based on provided context
+    const evaluated = await evaluateFlags(definitions, context);
+    const flags = flagsClient(evaluated.toggles);
+
+    // Check if the `EnableImpel` flag is turned on for this client
+    const isImpel = flags.isEnabled("EnableImpel");
+
+    const newUrl = req.nextUrl.clone();
+    // Redirect to variant
+    newUrl.pathname = `/ab/${isImpel ? "a" : "b"}`;
+    res = NextResponse.rewrite(newUrl);
+  } else {
+    res = NextResponse.next();
+  }
+  res.cookies.set(UNLEASH_COOKIE_NAME, sessionId);
+  return res;
 }
-      
