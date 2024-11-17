b8e0cfee72c82cb68a6edbf288224fc8f714af1a
Maikel <maikel.van.dort@gmail.com>
2024-11-17 20:27:17 +0100

    feat: flagsClient custom config

--- lib/src/flagsClient.ts
+++ lib/src/flagsClient.ts
@@ -1,27 +1,37 @@
 import { UnleashClient, type IToggle } from "unleash-proxy-client";
-import { getDefaultClientConfig, getServerBaseUrl } from "./utils";
+import {
+  getDefaultClientConfig,
+  getDefaultServerConfig,
+  getServerBaseUrl,
+} from "./utils";
 
 const getMetricsConfig = () => {
-  const serverUrl = getServerBaseUrl()
+  const serverUrl = getServerBaseUrl();
 
-  if (serverUrl && process.env.UNLEASH_SERVER_API_TOKEN){
-    return {
-      ...getDefaultClientConfig(),
-      url: serverUrl,
-      clientKey: process.env.UNLEASH_SERVER_API_TOKEN,
-    }
+  if (serverUrl && process.env.UNLEASH_SERVER_API_TOKEN) {
+    return getDefaultServerConfig();
   }
 
-  return getDefaultClientConfig()
-}
+  return getDefaultClientConfig();
+};
 
 /**
  * Simplified client SDK to work offline with pre-evaluated flags
  */
-export const flagsClient = (toggles = [] as IToggle[]) => {
-  const client = new UnleashClient({
-    bootstrap: toggles,
+export const flagsClient = (
+  toggles = [] as IToggle[],
+  config?: Partial<ReturnType<typeof getMetricsConfig>>
+) => {
+  const { appName, url, clientKey } = {
     ...getMetricsConfig(),
+    ...(config || {}),
+  };
+
+  const client = new UnleashClient({
+    url,
+    appName,
+    clientKey,
+    bootstrap: toggles,
     createAbortController: () => null,
     refreshInterval: 0,
     metricsInterval: 0,
