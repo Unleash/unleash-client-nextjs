a89425966f36d08959d2c39a2542bd6e8a894a6c
Thomas Heartman <thomasheartman+github@gmail.com>
2025-01-16 13:49:00 +0100

    feat: add sdk uniqueness headers (#101)
    Updates the dependency on the underlying Unleash client to 3.7.2. This
    version of the client adds new x-unleash headers which lets Unleash
    count unique connections and see SDK version information.
    
    Also updates the node client to 6.4.1 which includes the new headers for the server side (even though next uses it's own fetching).

--- lib/src/flagsClient.test.ts
+++ lib/src/flagsClient.test.ts
@@ -125,6 +125,9 @@ describe("flagsClient", () => {
         Authorization: 'a-very-nice-very-secure-custom-key',
         Accept: 'application/json',
         'Content-Type': 'application/json',
+        'x-unleash-appname': 'custom-app-name',
+        'x-unleash-connection-id': expect.stringMatching(/[a-f0-9-]{36}/),
+        'x-unleash-sdk': expect.stringMatching(/^unleash-js@\d\.\d\.\d/),
       },
       body: expect.stringContaining('custom-app-name'),
     }));
